daaab5afb2a182220fa3c6bbb6a8d73c34362489
Litos <windsochi@gmail.com>
2014-08-08 11:09:29 +0400

    fix style

--- sites/all/themes/bootstrap/bootstrap_subtheme/templates/html.tpl.php
+++ sites/all/themes/bootstrap/bootstrap_subtheme/templates/html.tpl.php
@@ -59,11 +59,15 @@
   <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
+<div id="wrapper-proc">
+
   <div id="skip-link">
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+  
+</div>
 </body>
 </html>
